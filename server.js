e2361632b1265d19b0d4b87d39c93ca79ce00dce
dlavaury <dlavaury@student.42.fr>
2019-02-04 11:18:19 +0100

    modif serve

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http');
-const visitedAction = (req, res) => {
-    res.writeHead(200);
-    res.end('Hello world !');
-};
-const server = http.createServer(visitedAction);
+const server = http.createServer(
+    (req, res) => {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(
+            '<!DOCTYPE html>' +
+            '<html>' +
+            '    <head>' +
+            '        <meta charset="utf-8" />' +
+            '        <title>Ma page Node.js !</title>' +
+            '    </head>' +
+            '    <body>' +
+            '     	<p>Hello <strong>WORLD</strong> !</p>' +
+            '    </body>' +
+            '</html>'
+        );
+        res.end();
+    }
+);
 
 server.listen(8080);
