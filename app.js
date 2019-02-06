243ae33502dc5857c2c83197f8209720f65cb4b4
dlavaury <dlavaury@student.42.fr>
2019-02-06 16:44:01 +0100

    opti code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 /*   By: dlavaury <dlavaury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 23:46:13 by dlavaury          #+#    #+#             */
-/*   Updated: 2019/02/06 00:25:27 by dlavaury         ###   ########.fr       */
+/*   Updated: 2019/02/06 16:43:11 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,33 @@ const io		= require('socket.io').listen(server);
 const todolist	= ['test', 'test1'];
 let id			= 0;
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'))
-.get('/css/style.css', (req, res) => res.sendFile(__dirname + '/css/style.css'))
-.get('/js/script.js', (req, res) => res.sendFile(__dirname + '/js/script.js'))
+app.get('/', (req, res) => res.sendFile(`${__dirname}/html/index.html`))
+.get('/css/style.css', (req, res) => res.sendFile(`${__dirname}/css/style.css`))
+.get('/js/script.js', (req, res) => res.sendFile(`${__dirname}/js/script.js`))
 .use((req, res, next) => res.redirect('/'));
 
-// quand un client ce connecte
 io.sockets.on('connection', socket => {
 	socket.name	= ++id;
 
 	console.log(`new client :  ${socket.name}`);
 
-	// send the current todolist to the client
 	socket.emit('current', JSON.stringify(todolist));
-	// when a client want to remove a todo element
 	socket.on('del', key => {
-		// chaque element est identifie par une key : son contenue
 		index	= todolist.indexOf(key);
 		if (index !== -1)
 		{
 			console.log(`client ${socket.name} remove : ${key}`);
-			// remove the element
+
 			todolist.splice(index, 1);
-			// send new list to all other client
 			socket.broadcast.emit('current', JSON.stringify(todolist));
 		}
 	})
-	// when a client want to add a element
 	.on('add', element => {
-		element	= ent.encode(element); // protect form script injection
+		element	= ent.encode(element);
 
 		console.log(`client ${socket.name} add ${element}`);
 
-		// add the new element
 		todolist.push(element);
-		// send notif to all other client
 		socket.broadcast.emit('add', element);
 	});
 });
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 /*   By: dlavaury <dlavaury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 00:09:33 by dlavaury          #+#    #+#             */
-/*   Updated: 2019/02/06 00:26:41 by dlavaury         ###   ########.fr       */
+/*   Updated: 2019/02/06 16:42:14 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@ const form			= document.getElementById('new-todo');
 const socket		= io.connect('http://127.0.0.1:8080');
 const id			= [];
 
-// function that create a new 'list element'
 function 			newTodo(text) {
 	const li		= document.createElement('li');
 	const a			= document.createElement('a');
@@ -31,35 +30,28 @@ function 			newTodo(text) {
 }
 
 form.addEventListener('submit', e => {
-	// prevent the form to reload the page
 	e.preventDefault();
-	// send the new todo to server
 	socket.emit('add', form.todo.value);
 	todo.appendChild(newTodo(form.todo.value));
-	// reset the field
 	form.reset();
 });
 
 todo.addEventListener('click', e => {
-	// get the click trough propagation
-	if (e.target.type === 'remove') { // only the element 'a' of type 'remove'
-		// send info to the server
+	if (e.target.type === 'remove') {
 		socket.emit('del', e.target.parentNode.children[1].textContent);
-		// remove the list
 		todo.removeChild(e.target.parentNode);
 	}
 	e.stopPropagation();
 });
 
-// when the server send a new todolist
 socket.on('current', todolist => {
-	todolist = JSON.parse(todolist);
-	// remove all child of to
+	todolist		= JSON.parse(todolist);
+	let i			= -1;
+
 	while (todo.firstChild) {
 		todo.removeChild(todo.firstChild);
 	}
-	// for each element in todolist create a new element to add to 'ul'
-	for (let i = 0; i < todolist.length; i++) {
+	while (++i < todolist.length) {
 		todo.appendChild(newTodo(todolist[i]));
 	}
 }).on('add', element => todo.appendChild(newTodo(element)));
